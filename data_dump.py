de7113a477b0d0e58d2c3f80b1ea470cd25b9535
upasanasaha <upasanasha40@gmail.com>
2023-01-18 18:47:47 +0530

    Project deployment configured.

--- data_dump.py
+++ data_dump.py
@@ -1,9 +1,7 @@
 import pymongo
 import pandas as pd
 import json
-
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+from sensor.config import mongo_client
 
 DATA_FILE_PATH="/config/workspace/aps_failure_training_set1.csv"
 DATABASE_NAME="aps"
@@ -18,4 +16,4 @@ if __name__ =="__main__":
     json_record = list(json.loads(df.T.to_json()).values())
     print(json_record[0])
 
-    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+    mongo_client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
